53a8d71121b39197b31ee5fe8243f0c2d579fba9
Petarj123 <pjankovic03@gmail.com>
2023-06-16 16:24:31 +0200

    Update ChatWidget.dart

--- chat_frontend/lib/widgets/ChatWidget.dart
+++ chat_frontend/lib/widgets/ChatWidget.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:shared_preferences/shared_preferences.dart';
